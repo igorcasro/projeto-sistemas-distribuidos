487cf8e61c01af8b77693014bf7dba7240054b29
Igor Castro <igorpfcastro@gmail.com>
2023-05-25 14:52:09 -0300

    fixed some frames to just dispose when closed, not exit the program

--- DB_GUI_SistemasDistribuidos/src/gui/client/SolicitarListaDeIncidentes.java
+++ DB_GUI_SistemasDistribuidos/src/gui/client/SolicitarListaDeIncidentes.java
@@ -125,7 +125,7 @@ public class SolicitarListaDeIncidentes extends JFrame {
 	public void initComponents() {
 		
 		setTitle("Solicitar Lista de Incidentes");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 416, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
